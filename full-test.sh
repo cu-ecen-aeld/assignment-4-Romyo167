656c8f64f3af4b9c345f58d4b2bad2184f91a282
Ramy Nosir <52046932+Romyo167@users.noreply.github.com>
2023-09-14 18:53:37 +0300

    Update full-test.sh

--- full-test.sh
+++ full-test.sh
@@ -4,10 +4,6 @@
 # steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
 set -e
 
-ls /usr/local/arm-cross-compiler/install/
-toolchain=/usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/
-export PATH=$PATH:$toolchain/bin/
-
 cd `dirname $0`
 test_dir=`pwd`
 echo "starting test with SKIP_BUILD=\"${SKIP_BUILD}\" and DO_VALIDATE=\"${DO_VALIDATE}\""
